d2ded5e10a8a68c9533a65107d12641d72b60354
ulises <ulisescamps9@gmail.com>
2019-09-09 19:37:16 -0500

    Validacion del menu principal

---
+++ src/components/Molecules/PrivateMenu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {NavLink} from "react-router-dom"
+
+const removeToken = () => {
+  localStorage.removeItem('token')
+  window.location = "/login"
+}
+
+const PrivateMenu = () => {
+  return (
+    <ul>
+                <li><NavLink exact to="/">Incio</NavLink></li>
+                <li><NavLink to="/especialidades">Especialidades</NavLink></li>
+                <li><NavLink to="/cursos">Cursos</NavLink></li>
+                <li><NavLink to="/profesores">Profesores</NavLink></li>
+                <li><NavLink onClick={() => removeToken()}>Cerrar Sesión</NavLink></li>
+            </ul>
+  )
+}
+
+export default PrivateMenu
--- src/components/Organisms/Header.jsx
+++ src/components/Organisms/Header.jsx
@@ -1,12 +1,11 @@
 import React, {createRef} from 'react'
 import { NavLink } from "react-router-dom"
+import PublicMenu from '../Molecules/PublicMenu';
+import PrivateMenu from '../Molecules/PrivateMenu';
 
 const menu = createRef()
 const toggleMenu = () => menu.current.classList.toggle('show')
-const removeToken = () => {
-  localStorage.removeItem('token')
-  window.location = "/login"
-}
+
 const Header = () => (
     <header className="main-header">
     <div className="ed-grid s-grid-5 lg-grid-4">
@@ -21,13 +20,11 @@ const Header = () => (
         </div>
         <div className="s-cols-1 lg-cols-3 s-cross-center s-main-end">
         <nav className="main-menu" ref={menu}>
-            <ul>
-                <li><NavLink exact to="/">Incio</NavLink></li>
-                <li><NavLink to="/especialidades">Especialidades</NavLink></li>
-                <li><NavLink to="/cursos">Cursos</NavLink></li>
-                <li><NavLink to="/profesores">Profesores</NavLink></li>
-                <li><NavLink onClick={() => removeToken()}>Cerrar Sesión</NavLink></li>
-            </ul>
+          {
+            localStorage.getItem('token') ?
+            <PrivateMenu/>
+            :<PublicMenu/>
+          }     
         </nav>
         <div className="main-menu-toggle to-l"
         onClick={() => toggleMenu()}
