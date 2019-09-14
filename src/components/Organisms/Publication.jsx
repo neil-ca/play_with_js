9d940bb7b8943eea083aeed535913b46c17e5df0
ulises <ulisescamps9@gmail.com>
2019-09-13 22:22:04 -0500

    Diseñando el home y la comunidad

---
+++ src/components/Organisms/Publication.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Publication = ({title, author, fecha, content}) => {
+  return (
+    <article className="publicattion l-block">
+      <div className="publication__container">
+        <h2 className="publication__title t3">{title}</h2>
+        <span> <b>{author}</b> a las {fecha}</span>
+        <div className="publication__content"></div>
+        <p>{content}</p>
+      </div>
+    </article>
+
+  )
+  
+}
+
+export default Publication
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,15 +1,63 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Banner from "../Organisms/Banner"
+import { connect } from "react-redux"
+import store from '../../redux/store';
+import { getAllPosts } from '../../redux/actionCreators'
+import Publication from "../Organisms/Publication";
 
-const Home = () => (
-    
-      <Banner
+const Home = ({ posts }) => {
+
+
+  useEffect(() => {
+    store.dispatch(getAllPosts())
+  }, [])
+  return (   
+      <>
+        <Banner
         color="dark-color"
         image="https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-08/IMG_20190517_121318_0.jpg"
         title="Bienvenido a la experiencia más increible en educación online"
         subtitle="Sé parte de esta comunidad y construyamos un mejor futuro"
         home
       />
-  )
+      <main className="ed-grid m-grid-3">
+        <div className="l-section m-cols-2">
+          <h2>Ultimas publicaciones</h2>
+        {
+          posts ?
+          <div>
+            {
+              posts.map(p => 
+              <Publication 
+              title={p.title}
+              author={p.author}
+              fecha={p.fecha}
+              content={p.content}
+              key={p.id}
+              />)
+            }
+          </div> :   
+          <p>No existen publicaciones</p>
+       
+      }
+      </div>  
+        <div>
+          <h3> Lista de categorias </h3>
+            <ul className="feature-list">
+              <li><span>React.js</span></li>
+              <li><span>React native</span></li>
+              <li><span>Angular</span></li>
+              <li><span>Vue.js</span></li>
+              <li><span>PHP</span></li>
 
-export default Home
+            </ul>
+        </div>
+      </main>
+      </>
+  )}
+
+  const mapStateToProps = state => ({
+    posts: state.postReducer.posts
+  })
+
+export default connect(mapStateToProps, {}) (Home)
