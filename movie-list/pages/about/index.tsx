c2f2e968bb9eedd16738922fe5393309a9340438
ulises <ulisescamps9@gmail.com>
2021-02-18 23:49:33 -0600

    using typescript and next

--- movie-list/pages/about/index.tsx
+++ movie-list/pages/about/index.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
-
+import styles from './about.module.css'
 function About() {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1 className={styles.title}>Hello</h1>
             <Link href="/"><a>|Go to home|</a></Link>
         </div>
     )
